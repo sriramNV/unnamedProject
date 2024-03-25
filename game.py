69e2b92a436dda322ba109bfa4f67973ba786cc3
Sriram <sriramnvilvanathan@gmail.com>
2024-03-26 01:34:16 +0530

    basic pygame screen

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.init()
+
+pygame.display.set_caption("UNNAMED PROJECT")
+screen = pygame.display.set_mode((640, 480))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
