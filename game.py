c62ef43d95fc52f021d3ede8d8ca7a11315c6cf9
Sriram <sriramnvilvanathan@gmail.com>
2024-03-26 02:41:15 +0530

    basic movement

--- game.py
+++ game.py
@@ -11,12 +11,32 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
+        self.cloud = pygame.image.load("./data/images/clouds/cloud_1.png")
+        self.cloud.set_colorkey((0, 0, 0))
+        self.cloud_pos = [160, 260]
+        self.movement = [False, False]
+
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+
+            self.cloud_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.cloud, self.cloud_pos)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
 
             pygame.display.update()
             self.clock.tick(60)
