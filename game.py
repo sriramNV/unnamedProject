fb00435081d0bd9232ce7ab56b4f65e682d74ca2
Sriram <sriramnvilvanathan@gmail.com>
2024-03-26 01:50:48 +0530

    updated the window as class obj

--- game.py
+++ game.py
@@ -1,18 +1,25 @@
 import pygame
 import sys
 
-pygame.init()
 
-pygame.display.set_caption("UNNAMED PROJECT")
-screen = pygame.display.set_mode((640, 480))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-clock = pygame.time.Clock()
+        pygame.display.set_caption("UNNAMED PROJECT")
+        self.screen = pygame.display.set_mode((640, 480))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
-    clock.tick(60)
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
