18724ec66cf0370b3ca087f07ed464b8f993786e
Sriram <sriramnvilvanathan@gmail.com>
2024-03-29 20:23:16 +0530

    added leveleditor and updated tilemap

--- game.py
+++ game.py
@@ -39,6 +39,7 @@ class Game:
         self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, tileSize=16)
+        self.tilemap.load("map.json")
 
         self.scroll = [0, 0]
 
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,4 @@
+import json
 import pygame
 
 
@@ -15,6 +16,20 @@ NEIGHBOUR_OFFSETS = [
 
 PHYSICS_TILE = {"grass", "stone"}
 
+AUTOTILE_MAP = {
+    tuple(sorted([(1, 0), (0, 1)])): 0,
+    tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+    tuple(sorted([(-1, 0), (0, 1)])): 2,
+    tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+    tuple(sorted([(-1, 0), (0, -1)])): 4,
+    tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+    tuple(sorted([(1, 0), (0, -1)])): 6,
+    tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8,
+}
+
+AUTOTILE_TYPES = {"grass", "stone"}
+
 
 class Tilemap:
     def __init__(self, game, tileSize=16):
@@ -22,18 +37,6 @@ class Tilemap:
         self.tileSize = tileSize
         self.tilemap = {}
         self.offgridTiles = []
-
-        for i in range(10):
-            self.tilemap[str(3 + i) + ";10"] = {
-                "type": "grass",
-                "variant": 1,
-                "pos": (3 + i, 10),
-            }
-            self.tilemap["10;" + str(5 + i)] = {
-                "type": "stone",
-                "variant": 1,
-                "pos": (10, 5 + i),
-            }
 
     def tilesAround(self, pos):
         tiles = []
@@ -46,6 +49,12 @@ class Tilemap:
         return tiles
 
     def render(self, surface, offset=(0, 0)):
+        for tile in self.offgridTiles:
+            surface.blit(
+                self.game.assets[tile["type"]][tile["variant"]],
+                (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]),
+            )
+
         for x in range(
             offset[0] // self.tileSize,
             (offset[0] + surface.get_width()) // self.tileSize + 1,
@@ -65,12 +74,7 @@ class Tilemap:
                         ),
                     )
 
-    # for tile in self.offgridTiles:
-    # surface.blit(
-    #     self.game.assets[tile["type"]][tile["variant"]],
-    #     (tile["pos"][0] - offset[0].tile["pos"][1] - offset[1]),
-    # )
-
+    # not needed anymore
     # for location in self.tilemap:
     #     tile = self.tilemap[location]
     #     surface.blit(
@@ -94,3 +98,41 @@ class Tilemap:
                     )
                 )
         return rects
+
+    def save(self, path):
+        f = open(path, "w")
+        json.dump(
+            {
+                "tilemap": self.tilemap,
+                "tile_size": self.tileSize,
+                "offgrid": self.offgridTiles,
+            },
+            f,
+        )
+        f.close()
+
+    def load(self, path):
+        f = open(path, "r")
+        map_data = json.load(f)
+        f.close()
+
+        self.tilemap = map_data["tilemap"]
+        self.tileSize = map_data["tile_size"]
+        self.offgridTiles = map_data["offgrid"]
+
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = (
+                    str(tile["pos"][0] + shift[0])
+                    + ";"
+                    + str(tile["pos"][1] + shift[1])
+                )
+                if check_loc in self.tilemap:
+                    if self.tilemap[check_loc]["type"] == tile["type"]:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if (tile["type"] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
+                tile["variant"] = AUTOTILE_MAP[neighbors]
