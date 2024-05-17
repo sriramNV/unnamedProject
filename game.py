d8d19c3c17f48ce39b3012bb4417b2a2db54beca
Sriram <sriramnvilvanathan@gmail.com>
2024-05-17 19:29:24 +0530

    updating dash mechanics

--- game.py
+++ game.py
@@ -120,6 +120,9 @@ class Game:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
                         self.player.jump()
+                    if event.key == pygame.K_x:
+                        self.player.dash()
+
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
